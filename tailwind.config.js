1f3aa0e2a6f7ede1dbad2d79b20fab7bd1ee9ff2
AnhtuanUit <AnhtuanUit>
2024-05-13 08:45:20 +0000

    Pushed from Locofy Builder

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,3 +28,33 @@ module.exports = {
     preflight: false,
   },
 };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "soft-grey": "#e7e7e7",
+        white: "#fff",
+        red: "#e73539",
+        black: "#000",
+        grey: "#444343",
+      },
+      spacing: {},
+      fontFamily: {
+        menu: "'Saira Condensed'",
+        body: "Inter",
+      },
+    },
+    fontSize: {
+      "5xl": "1.5rem",
+      base: "1rem",
+      "31xl": "3.125rem",
+      "14xl": "2.063rem",
+      inherit: "inherit",
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
