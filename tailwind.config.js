b221c2f4b29573eeed34fc935f12d6439f155db0
AnhtuanUit <AnhtuanUit>
2024-05-13 11:15:50 +0000

    Pushed from Locofy Builder

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     extend: {
       colors: {
         "soft-grey": "#e7e7e7",
-        white: "#fff",
         red: "#e73539",
+        white: "#fff",
         black: "#000",
         grey: "#444343",
       },
@@ -17,40 +17,10 @@ module.exports = {
       },
     },
     fontSize: {
-      "5xl": "1.333rem",
-      base: "0.889rem",
-      "31xl": "2.778rem",
-      "14xl": "1.833rem",
-      inherit: "inherit",
-    },
-  },
-  corePlugins: {
-    preflight: false,
-  },
-};
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "soft-grey": "#e7e7e7",
-        white: "#fff",
-        red: "#e73539",
-        black: "#000",
-        grey: "#444343",
-      },
-      spacing: {},
-      fontFamily: {
-        menu: "'Saira Condensed'",
-        body: "Inter",
-      },
-    },
-    fontSize: {
-      "5xl": "1.5rem",
       base: "1rem",
-      "31xl": "3.125rem",
       "14xl": "2.063rem",
+      "31xl": "3.125rem",
+      "5xl": "1.5rem",
       inherit: "inherit",
     },
   },
